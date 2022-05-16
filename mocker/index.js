01d121e82eede8a2d9e700f25a37a5666f974f84
huqiaoli <huqiaoli@jz-sh.cn>
2022-05-16 16:54:50 +0800

    fix: 调试登录接口

--- mocker/index.js
+++ mocker/index.js
@@ -1,26 +1,11 @@
-const { login, verify, logout, reloadAuth } = require('./login')
-const { selectPage, milestonesSave } = require('./milestone')
-const { allusersqueryByPage } = require('./allusers')
-const { dictqueryByPage, dictqueryAll } = require('./dictionary')
-const {
-  managerAssignmentselectPage,
-  managerAssignmentselectById,
-} = require('./project')
-
 const proxy = {
-  'GET /api/user': { id: 1, username: 'kenny', sex: 6 },
-  'POST /api/user': { id: 1, username: 'kenny', sex: 6 },
-  'POST /api/login': login,
-  'POST /api/logout': logout,
-  'GET /api/user/verify': verify,
-  'POST /api/reloadAuth': reloadAuth,
-  'POST /api/milestones/selectPage': selectPage,
-  'POST /api/milestones/milestonesSave': milestonesSave,
-  'POST /api/managerUser/queryByPage': allusersqueryByPage,
-  'POST /api/dict/queryByPage': dictqueryByPage,
-  'POST /api/dict/queryAll': dictqueryAll,
-  'POST /api/ManagerAssignment/selectPage': managerAssignmentselectPage,
-  'POST //api/ManagerAssignment/selectById': managerAssignmentselectById,
+  _proxy: {
+    proxy: {
+      // '/api(.*)': 'http://192.168.188.61:8083/',
+      '/api(.*)': 'https://dtpms-dev.nihaosi.com',
+    },
+    changeHost: true,
+  },
 }
 
 module.exports = proxy
