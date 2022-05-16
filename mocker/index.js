b35c0c77806ca31ddaa55c5203a15cffd2df0508
gssggssg <490104722@qq.com>
2022-05-16 12:37:47 +0800

    test: 增加mockr数据

--- mocker/index.js
+++ mocker/index.js
@@ -1,6 +1,5 @@
-const { getData, getCity } = require('./selectPage')
 const { login, verify, logout, reloadAuth } = require('./login')
-const { insert, update, selectById, upload } = require('./demo')
+const { selectPage, milestonesSave } = require('./milestone')
 
 const proxy = {
   'GET /api/user': { id: 1, username: 'kenny', sex: 6 },
@@ -8,13 +7,9 @@ const proxy = {
   'POST /api/login': login,
   'POST /api/logout': logout,
   'GET /api/user/verify': verify,
-  'GET /api/city': getCity,
-  'POST /api/getData': getData,
   'POST /api/reloadAuth': reloadAuth,
-  'POST /api/demo/selectById': selectById,
-  'POST /api/demo/insert': insert,
-  'POST /api/demo/update': update,
-  'POST /api/demo/upload': upload,
+  'POST /api/milestones/selectPage': selectPage,
+  'POST /api/milestones/milestonesSave': milestonesSave,
 }
 
 module.exports = proxy
