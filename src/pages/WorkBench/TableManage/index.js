9897b7e87896eb2bb05bfa86949de37608859472
Panbibi <1265629139@qq.com>
2022-05-16 19:34:28 +0800

    feat: 添加成员动态卡片 #277

--- src/pages/WorkBench/TableManage/index.js
+++ src/pages/WorkBench/TableManage/index.js
@@ -8,7 +8,7 @@ export default function TableManage() {
     <div style={{ marginTop: 5 }}>
       <Split
         style={{ height: 400, border: '1px solid #d5d5d5', borderRadius: 3 }}>
-        <div style={{ width: '70%', minWidth: 30 }}>
+        <div style={{ width: '100%', minWidth: 30 }}>
           <Card style={{ height: '100%' }}>
             <Tabs
               type="line"
@@ -25,16 +25,12 @@ export default function TableManage() {
               <Tabs.Pane sequence="fadeIn up" label="已逾期" key="3">
                 <Overdue />
               </Tabs.Pane>
-              {/* <Tabs.Pane label="更多" style={{marginRight:10}}>
-              </Tabs.Pane> */}
+              <Tabs.Pane
+                label="更多"
+                style={{ marginRight: 10 }}
+                key="4"></Tabs.Pane>
             </Tabs>
           </Card>
-        </div>
-        <div style={{ width: '30%', minWidth: 10 }}>
-          {/* <Card
-            title="成员动态"
-            bordered={false}
-            style={{ height: '100%' }}></Card> */}
         </div>
       </Split>
     </div>
--- src/pages/WorkBench/TableUser/index.js
+++ src/pages/WorkBench/TableUser/index.js
@@ -3,13 +3,51 @@ import Pendings from './Pendings'
 import Ongoing from './Ongoing'
 import Overdue from './Overdue'
 import OurCreate from './OurCreate'
+import { ShowSteps } from '@/components'
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
 export default function TableUser() {
+  const dispatch = useDispatch()
+  const {
+    workbench: { listDynamic, taskId },
+  } = useSelector((state) => state)
+  useEffect(() => {
+    // 优先判断是否存在ID
+    dispatch({
+      type: 'workbench/selectOperatingRecord',
+      payload: { projectId: taskId },
+    })
+  }, [taskId, dispatch])
+
+  const listDynamicData = () => {
+    const newData = listDynamic?.map((item) => {
+      const date = item?.slice(0, item?.indexOf(' '))
+      const time = item?.slice(item?.indexOf(' ') + 1, item?.indexOf(','))
+      const text = item?.substr(item?.indexOf(',') + 1)
+      return { date, time, text }
+    })
+    let textArr = [],
+      newDate = ''
+    return newData
+      .map((item, index) => {
+        if (item?.date !== newDate) {
+          newDate = item.date
+          textArr = []
+        }
+        textArr.push(item)
+        if (newData[index + 1]?.date !== newDate)
+          return { ...item, dayDynamics: textArr }
+        return undefined
+      })
+      .filter((s) => s)
+  }
+
   return (
     <div style={{ marginTop: 5 }}>
       <Split
         style={{ height: 400, border: '1px solid #d5d5d5', borderRadius: 3 }}>
-        <div style={{ width: '70%', minWidth: 30 }}>
+        <div style={{ width: '60%', minWidth: 30 }}>
           <Card style={{ height: '100%' }}>
             <Tabs
               type="line"
@@ -33,11 +71,13 @@ export default function TableUser() {
             </Tabs>
           </Card>
         </div>
-        <div style={{ width: '30%', minWidth: 100, height: '100%' }}>
-          <Card
-            title="成员动态"
-            bordered={false}
-            style={{ height: '100%' }}></Card>
+        <div style={{ width: '40%', minWidth: 100, height: '100%' }}>
+          <Card title="成员动态" bordered={false} style={{ height: '100%' }}>
+            <div
+              style={{ height: 320, overflowX: 'hidden', overflowY: 'auto' }}>
+              <ShowSteps listData={listDynamicData()} size="small" />
+            </div>
+          </Card>
         </div>
       </Split>
     </div>
