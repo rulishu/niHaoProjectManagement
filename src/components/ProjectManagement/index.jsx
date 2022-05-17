3bce304cf15c692cc3a83f424cdffb0cee6a31bf
夏佳乐 <xiajiale@jz-sh.cn>
2022-05-17 19:32:11 +0800

    feat: 更改其样式 #287

--- src/components/ProjectManagement/index.jsx
+++ src/components/ProjectManagement/index.jsx
@@ -2,7 +2,6 @@ import { useDispatch, useSelector } from 'react-redux'
 import { ProDrawer, ProForm, useForm } from '@uiw-admin/components'
 import { Notify } from 'uiw'
 import useSWR from 'swr'
-import { item } from './items'
 
 /**
  * 使用方法：
@@ -21,11 +20,11 @@ import { item } from './items'
  * @returns
  */
 
-const ProjectManagement = ({ onSearch }) => {
+const ProjectManagement = () => {
   const baseRef = useForm()
   const dispatch = useDispatch()
   const {
-    global: { drawerVisible, drawerType, seachValue, isView },
+    global: { drawerVisible, drawerType, seachValue },
   } = useSelector((state) => state)
 
   const updateData = (payload) => {
@@ -39,15 +38,16 @@ const ProjectManagement = ({ onSearch }) => {
     updateData({
       drawerVisible: false,
       seachValue: {},
-      isView: false,
     })
   }
 
   const { mutate, isValidating } = useSWR(
     [
       (drawerType === 'edit' && '/api/project/update') ||
-        (drawerType === 'add' && '/api/project/add'),
-      { method: 'POST', body: seachValue },
+        (drawerType === 'add' && '/api/project/add') || {
+          method: 'POST',
+          body: seachValue,
+        },
     ],
     {
       revalidateOnMount: false,
@@ -55,8 +55,6 @@ const ProjectManagement = ({ onSearch }) => {
       onSuccess: (data) => {
         if (data && data.code === 200) {
           Notify.success({ title: data.msg })
-          onClose()
-          onSearch?.()
         } else {
           Notify.error({ title: data.msg })
         }
@@ -73,34 +71,88 @@ const ProjectManagement = ({ onSearch }) => {
         title={drawerType === 'edit' ? '编辑' : '新增'}
         buttons={[
           {
+            label: '取消',
+            onClick: onClose,
+          },
+          {
             label: '保存',
-            style: { width: 80 },
             type: 'primary',
-            show: !isView,
             loading: isValidating,
             onClick: async () => {
               await baseRef?.submitvalidate?.()
               const errors = baseRef.getError()
-              if (errors && Object.keys(errors).length > 0) {
-                return
-              }
+              if (errors && Object.keys(errors).length > 0) return
               mutate()
             },
-          },
-          {
-            label: isView ? '关闭' : '取消',
-            style: { width: 80 },
-            onClick: onClose,
           },
         ]}>
         <ProForm
           formType="pure"
           form={baseRef}
-          readOnly={isView}
+          formDatas={[
+            {
+              label: '项目名称:',
+              key: 'name',
+              widget: 'input',
+              initialValue: seachValue.name,
+              widgetProps: {},
+              placeholder: '请输入项目名称',
+              span: '24',
+              required: true,
+              rules: [{ required: true, message: '请输入项目名称' }],
+            },
+            {
+              label: '项目负责人:',
+              key: 'projectLeaderName',
+              widget: 'select',
+              option: [
+                { value: 1, label: '苹果' },
+                { value: 2, label: '西瓜' },
+                { value: 3, label: '香蕉' },
+                { value: 4, label: '东北大冻梨' },
+              ],
+              initialValue: seachValue.projectLeaderName,
+              widgetProps: {},
+              span: '24',
+              required: true,
+              rules: [{ required: true, message: '请输入项目负责人' }],
+            },
+            {
+              label: '起止日期:',
+              key: 'begin',
+              initialValue: seachValue.begin,
+              widget: 'dateInput',
+              widgetProps: {
+                format: 'YYYY-MM-DD HH:mm:ss',
+              },
+              span: '24',
+              required: true,
+              rules: [{ required: true, message: '请输入起止日期' }],
+            },
+            {
+              label: '截止日期:',
+              key: 'end',
+              initialValue: seachValue.end,
+              widget: 'dateInput',
+              widgetProps: {
+                format: 'YYYY-MM-DD HH:mm:ss',
+              },
+              span: '24',
+              required: true,
+              rules: [{ required: true, message: '请输入截止日期' }],
+            },
+            {
+              label: '项目描述:',
+              key: 'descr',
+              widget: 'textarea',
+              initialValue: seachValue.descr,
+              widgetProps: {},
+              span: '24',
+            },
+          ]}
           onChange={(initial, current) => {
-            updateData({ seachValue: { ...seachValue } })
+            updateData({ seachValue: { ...current } })
           }}
-          formDatas={item(seachValue)}
         />
       </ProDrawer>
     </div>
