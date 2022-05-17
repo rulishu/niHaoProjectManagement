ad882d9e1632c85e2f58cb3e074b8d481f232004
宋玉 <song@songyudeMacBook-Air.local>
2022-05-17 16:40:16 +0800

    feat: 【权限管理】岗位管理界面API调用 @295

--- src/pages/Authority/PostManagement/index.js
+++ src/pages/Authority/PostManagement/index.js
@@ -1,31 +1,37 @@
 import { Fragment } from 'react'
 import { ProTable, useTable } from '@uiw-admin/components'
-import { Alert } from 'uiw'
+import { Alert, Badge } from 'uiw'
 import { useDispatch, useSelector } from 'react-redux'
 import Drawer from './Drawer'
 import { searchFun } from '@/utils/publicFun'
 import operateFun from '@/components/Operate'
 
+const token = localStorage.getItem('token')
 export default function Index() {
   const dispatch = useDispatch()
   const {
     local: { alertShow, ids },
   } = useSelector((state) => state)
 
-  const table = useTable('/api/userDevice/selectPage', {
+  const table = useTable('/api/system/post/optionselect', {
     query: (pageIndex, pageSize, searchValues) => {
       return {
         page: pageIndex,
         pageSize: pageSize,
-        ...searchValues,
+        postName: searchValues?.postName || '',
+        status: searchValues?.status || '',
       }
     },
 
     formatData: (data) => {
       return {
         total: data?.data?.total || 0,
-        data: data?.data?.rows || [],
+        data: data?.data || [],
       }
+    },
+    requestOptions: {
+      method: 'GET',
+      headers: { Authorization: 'Bearer ' + token },
     },
   })
   const onOpenDelete = (data) => {
@@ -96,8 +102,15 @@ export default function Index() {
         ]}
         columns={[
           {
+            title: '岗位序号',
+            key: 'postId',
+            width: 130,
+            align: 'center',
+            ellipsis: true,
+          },
+          {
             title: '岗位名称',
-            key: 'deviceName',
+            key: 'postName',
             width: 130,
             align: 'center',
             ellipsis: true,
@@ -110,21 +123,21 @@ export default function Index() {
           },
           {
             title: '岗位编码',
-            key: 'deviceNo',
+            key: 'postCode',
             width: 130,
             align: 'center',
             ellipsis: true,
           },
           {
-            title: '岗位顺序',
-            key: 'deviceAddress',
+            title: '岗位排序',
+            key: 'postSort',
             width: 130,
             align: 'center',
             ellipsis: true,
           },
           {
-            title: '备注',
-            key: 'devicePurpose',
+            title: '岗位排序',
+            key: 'createTime',
             width: 130,
             align: 'center',
             ellipsis: true,
@@ -138,27 +151,26 @@ export default function Index() {
             props: {
               widget: 'select',
               option: [
-                { label: '正常', value: 1 },
-                { label: '停用', value: 2 },
+                { label: '正常', value: 0 },
+                { label: '停用', value: 1 },
               ],
               widgetProps: {
                 placeholder: '请输入岗位状态',
               },
             },
+            //
             render: (code) => {
-              return <div>{code === 1 ? '闲置' : '已出售'}</div>
+              return (
+                <div>
+                  <Badge color={code === 0 ? '#28a745' : '#c2c2c2'} />
+                  {code === 0 ? '正常' : '停用'}
+                </div>
+              )
             },
           },
           {
             title: '创建时间',
             key: 'createTime',
-            width: 130,
-            align: 'center',
-            ellipsis: true,
-          },
-          {
-            title: '警告次数',
-            key: 'warnNum',
             width: 130,
             align: 'center',
             ellipsis: true,
