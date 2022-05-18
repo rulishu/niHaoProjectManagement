d4a024572bcf479b97579053b2fd16e774545d4e
gssggssg <490104722@qq.com>
2022-05-18 23:53:50 +0800

    feat: 完善里程碑相关接口问题 #286

--- src/pages/Milestone/NewMilestone/index.jsx
+++ src/pages/Milestone/NewMilestone/index.jsx
@@ -1,58 +1,64 @@
 import { useEffect } from 'react'
 import { Row, Col, Input, DateInput, Select, Form, Button, Loader } from 'uiw'
-import { connect } from 'react-redux'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate, useParams } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
 import formatter from '@uiw/formatter'
 import { NEWMDEditor } from '@/components'
 import styles from './index.module.less'
+import timeDistance from '@/utils/timeDistance'
 
-const NewMilestone = (props) => {
-  const { milestoneType, listDataInfo, projectId, milestonesId } =
-    props.milestone
-  const { loading } = props
+const NewMilestone = () => {
+  const {
+    milestone: { milestoneType, listDataInfo },
+    loading,
+  } = useSelector((state) => state)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
   const { addMilestone, editMilestone, getMilestone } =
     loading.effects.milestone
 
   const location = useLocation()
-  // const pathArr = location.pathname.split('/')
+  const { projectId, milestonesId } = useParams()
+
+  useEffect(() => {
+    dispatch.milestone.update({ projectId, milestonesId })
+  }, [projectId, milestonesId, dispatch])
+
   useEffect(() => {
     const pathArr = location.pathname.split('/')
     if (!milestoneType) {
       if (pathArr.includes('editMilestone')) {
-        const milestonesId = pathArr[pathArr.length - 1]
-        const projectId = pathArr[pathArr.length - 2]
         const callback = async () => {
-          await props.dispatch.getMilestone({ projectId, milestonesId })
-          await props.dispatch.update({
-            milestoneType: 2,
-            projectId,
-            milestonesId,
-          })
+          await dispatch.milestone.update({ milestoneType: 2 })
         }
         callback()
       }
       if (pathArr.includes('newMilestone')) {
-        const projectId = pathArr[pathArr.length - 1]
-        props.dispatch.update({ listDataInfo: {}, projectId, milestoneType: 1 })
+        dispatch.milestone.update({
+          listDataInfo: {},
+          projectId,
+          milestoneType: 1,
+        })
       }
     }
-    if (milestoneType) {
-      if (milestoneType === 1) {
-        const projectId = pathArr[pathArr.length - 1]
-        props.dispatch.update({ listDataInfo: {}, projectId })
-      }
-      if (milestoneType === 2) {
-        const milestonesId = pathArr[pathArr.length - 1]
-        const projectId = pathArr[pathArr.length - 2]
-        props.dispatch.getMilestone(projectId, milestonesId)
-      }
-    }
-  }, [milestoneType, location, props.dispatch, projectId, milestonesId])
+    milestoneType === 1 &&
+      dispatch.milestone.update({ listDataInfo: {}, projectId })
+    milestoneType === 2 &&
+      dispatch.milestone.getMilestone({ projectId, milestonesId })
+  }, [
+    dispatch.milestone,
+    location.pathname,
+    milestoneType,
+    milestonesId,
+    projectId,
+  ])
 
   // 取消回调
   const onCancel = () => {
     window.history.back(-1)
   }
+
+  console.log('listDataInfo====>', listDataInfo, milestoneType)
 
   return (
     <div className={styles.main}>
@@ -72,156 +78,173 @@ const NewMilestone = (props) => {
               ? getMilestone || editMilestone
               : false
           }>
-          <Form
-            onSubmit={async ({ initial, current }) => {
-              const errorObj = {}
-              const { milestonesTitle, startTime, milestonesDesc } = current
-              if (
-                !milestonesTitle ||
-                milestonesTitle.length < 2 ||
-                milestonesTitle.length > 100
-              ) {
-                errorObj.milestonesTitle = '请输入标题,长度为2~100'
-              }
-              if (milestonesDesc && milestonesDesc.length > 300) {
-                errorObj.milestonesDesc = '描述内容长度为应西不大于300'
-              }
-              if (!startTime) {
-                errorObj.startTime = '开始时间不能为空！'
-              }
-              if (Object.keys(errorObj).length > 0) {
-                const err = new Error()
-                err.filed = errorObj
-                throw err
-              }
+          {Object.keys(listDataInfo).length ? (
+            <Form
+              onSubmit={async ({ initial, current }) => {
+                const errorObj = {}
+                const { milestonesTitle, startTime, milestonesDesc } = current
+                if (
+                  !milestonesTitle ||
+                  milestonesTitle.length < 2 ||
+                  milestonesTitle.length > 100
+                ) {
+                  errorObj.milestonesTitle = '请输入标题,长度为2~100'
+                }
+                if (milestonesDesc && milestonesDesc.length > 300) {
+                  errorObj.milestonesDesc = '描述内容长度为应西不大于300'
+                }
+                if (!startTime) {
+                  errorObj.startTime = '开始时间不能为空！'
+                }
+                if (!timeDistance(current.startTime, current.dueTime).status) {
+                  errorObj.dueTime = '里程碑开始时间不能大于里程碑的截至时间！'
+                }
+                if (Object.keys(errorObj).length > 0) {
+                  const err = new Error()
+                  err.filed = errorObj
+                  throw err
+                }
 
-              //提交
-              const { dispatch } = props
-              const param = {
-                ...current,
-                dueTime:
-                  current.dueTime &&
-                  formatter('YYYY-MM-DD', new Date(current.dueTime)),
-                startTime: formatter('YYYY-MM-DD', new Date(current.startTime)),
-                milestonesStatus: +current.milestonesStatus,
-                milestonesId: current.milestonesId || milestonesId,
-                projectId,
-              }
-              const params = { payload: param, callback: onCancel }
-              milestoneType === 1 && dispatch.addMilestone(params)
-              milestoneType === 2 && dispatch.editMilestone(params)
-            }}
-            onSubmitError={(error) => {
-              if (error.filed) {
-                return { ...error.filed }
-              }
-              return null
-            }}
-            fields={{
-              milestonesId: {
-                inline: true,
-                required: true,
-                initialValue: listDataInfo.milestonesId,
-                children: <Input />,
-              },
-              milestonesTitle: {
-                inline: true,
-                required: true,
-                initialValue: listDataInfo.milestonesTitle,
-                children: <Input placeholder="请输入标题" />,
-              },
-              startTime: {
-                initialValue: listDataInfo.startTime,
-                inline: true,
-                required: true,
-                labelFor: 'date-inline',
-                children: (
-                  <DateInput
-                    format="YYYY-MM-DD"
-                    datePickerProps={{ todayButton: '今天' }}
-                  />
-                ),
-              },
-              dueTime: {
-                initialValue: listDataInfo.dueTime,
-                inline: true,
-                labelFor: 'date-inline',
-                children: (
-                  <DateInput
-                    format="YYYY-MM-DD"
-                    datePickerProps={{ todayButton: '今天' }}
-                  />
-                ),
-              },
-              milestonesStatus: {
-                initialValue: listDataInfo.milestonesStatus || 1,
-                inline: true,
-                children: (
-                  <Select className={styles.fromSelect}>
-                    <Select.Option value={1}>打开</Select.Option>
-                    <Select.Option value={2}>关闭</Select.Option>
-                    <Select.Option value={3}>删除</Select.Option>
-                  </Select>
-                ),
-              },
-              milestonesDesc: {
-                initialValue: listDataInfo.milestonesDesc,
-                inline: true,
-                children: <NEWMDEditor rfval={(e) => console.log(e)} />,
-              },
-            }}>
-            {({ fields, state, canSubmit }) => {
-              return (
-                <div className={styles.from}>
-                  <Row align="baseline" className={styles.fromItem}>
-                    <Col span="4" className={styles.titleInput}>
-                      标题
-                    </Col>
-                    <Col span="19">{fields.milestonesTitle}</Col>
-                  </Row>
-                  <Row align="baseline" className={styles.fromItem}>
-                    <Col span="4" className={styles.titleInput}>
-                      开始时间
-                    </Col>
-                    <Col span="19">{fields.startTime}</Col>
-                  </Row>
-                  <Row align="baseline" className={styles.fromItem}>
-                    <Col span="4" className={styles.titleInput}>
-                      结束时间
-                    </Col>
-                    <Col span="19">{fields.dueTime}</Col>
-                  </Row>
-                  <Row align="baseline" className={styles.fromItem}>
-                    <Col span="4" className={styles.titleInput}>
-                      描述
-                    </Col>
-                    <Col span="19">{fields.milestonesDesc}</Col>
-                  </Row>
-                  <Row align="baseline" className={styles.fromButton}>
-                    <Col>
-                      <Button disabled={!canSubmit()} htmlType="submit">
-                        保存
-                      </Button>
-                      <Button onClick={() => onCancel()}>取消</Button>
-                    </Col>
-                  </Row>
-                </div>
-              )
-            }}
-          </Form>
+                //提交
+                const param = {
+                  ...current,
+                  dueTime:
+                    current.dueTime &&
+                    formatter('YYYY-MM-DD', new Date(current.dueTime)),
+                  startTime: formatter(
+                    'YYYY-MM-DD',
+                    new Date(current.startTime)
+                  ),
+                  milestonesStatus: +current.milestonesStatus,
+                  milestonesId: current.milestonesId || milestonesId,
+                  projectId,
+                }
+                if (milestoneType === 1) {
+                  delete param.milestonesId
+                  const listGo = (projectId, milestonesId) => {
+                    navigate(
+                      `/milestone/milestoneInfo/${projectId}/${milestonesId}`,
+                      {
+                        state: { projectId, milestonesId },
+                      }
+                    )
+                  }
+                  dispatch.milestone.addMilestone({
+                    payload: param,
+                    callback: listGo,
+                  })
+                }
+                milestoneType === 2 &&
+                  dispatch.milestone.editMilestone({
+                    payload: param,
+                    callback: onCancel,
+                  })
+              }}
+              onSubmitError={(error) => {
+                if (error.filed) {
+                  return { ...error.filed }
+                }
+                return null
+              }}
+              fields={{
+                milestonesId: {
+                  inline: true,
+                  required: true,
+                  initialValue: listDataInfo.milestonesId,
+                  children: <Input />,
+                },
+                milestonesTitle: {
+                  inline: true,
+                  required: true,
+                  initialValue: listDataInfo.milestonesTitle,
+                  children: <Input placeholder="请输入标题" />,
+                },
+                startTime: {
+                  initialValue: listDataInfo.startTime,
+                  inline: true,
+                  required: true,
+                  labelFor: 'date-inline',
+                  children: (
+                    <DateInput
+                      format="YYYY-MM-DD"
+                      datePickerProps={{ todayButton: '今天' }}
+                    />
+                  ),
+                },
+                dueTime: {
+                  initialValue: listDataInfo.dueTime,
+                  inline: true,
+                  labelFor: 'date-inline',
+                  children: (
+                    <DateInput
+                      format="YYYY-MM-DD"
+                      datePickerProps={{ todayButton: '今天' }}
+                    />
+                  ),
+                },
+                milestonesStatus: {
+                  initialValue: listDataInfo.milestonesStatus || 1,
+                  inline: true,
+                  children: (
+                    <Select className={styles.fromSelect}>
+                      <Select.Option value={1}>打开</Select.Option>
+                      <Select.Option value={2}>关闭</Select.Option>
+                      <Select.Option value={3}>删除</Select.Option>
+                    </Select>
+                  ),
+                },
+                milestonesDesc: {
+                  initialValue: listDataInfo.milestonesDesc,
+                  inline: true,
+                  children: <NEWMDEditor rfval={(e) => console.log(e)} />,
+                },
+              }}>
+              {({ fields, state, canSubmit }) => {
+                return (
+                  <div className={styles.from}>
+                    <Row align="baseline" className={styles.fromItem}>
+                      <Col span="4" className={styles.titleInput}>
+                        标题
+                      </Col>
+                      <Col span="19">{fields.milestonesTitle}</Col>
+                    </Row>
+                    <Row align="baseline" className={styles.fromItem}>
+                      <Col span="4" className={styles.titleInput}>
+                        开始时间
+                      </Col>
+                      <Col span="19">{fields.startTime}</Col>
+                    </Row>
+                    <Row align="baseline" className={styles.fromItem}>
+                      <Col span="4" className={styles.titleInput}>
+                        结束时间
+                      </Col>
+                      <Col span="19">{fields.dueTime}</Col>
+                    </Row>
+                    <Row align="baseline" className={styles.fromItem}>
+                      <Col span="4" className={styles.titleInput}>
+                        描述
+                      </Col>
+                      <Col span="19">{fields.milestonesDesc}</Col>
+                    </Row>
+                    <Row align="baseline" className={styles.fromButton}>
+                      <Col>
+                        <Button disabled={!canSubmit()} htmlType="submit">
+                          保存
+                        </Button>
+                        <Button onClick={() => onCancel()}>取消</Button>
+                      </Col>
+                    </Row>
+                  </div>
+                )
+              }}
+            </Form>
+          ) : (
+            <></>
+          )}
         </Loader>
       </div>
     </div>
   )
 }
 
-const mapStateToProps = ({ milestone, loading }) => ({
-  milestone: milestone,
-  loading: loading,
-})
-
-const mapDispatchToProps = ({ milestone }) => ({
-  dispatch: milestone,
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(NewMilestone)
+export default NewMilestone
