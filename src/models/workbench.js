61f8bdc172d8f499709e4562dfff6031529e3885
Panbibi <1265629139@qq.com>
2022-05-18 18:46:29 +0800

    feat: 【工作台】成员动态接口联调 #302

--- src/models/workbench.js
+++ src/models/workbench.js
@@ -1,8 +1,9 @@
 import { createModel } from '@rematch/core'
 import {
-  selectOperatingRecord,
   myProject,
   memberOperator,
+  selectAllProjectPage,
+  selectProjectPage,
 } from '../servers/workbench'
 import { Notify } from 'uiw'
 
@@ -11,22 +12,15 @@ const workbench = createModel()({
   state: {
     listDynamic: [],
     projectList: [],
-    taskId: '1594',
+    memberList: [],
+    allList: [],
+    personList: [],
   },
   effects: (dispatch) => ({
-    // 查询项目的操作动态
-    async selectOperatingRecord(payload) {
-      const data = await selectOperatingRecord(payload)
-      if (data.code === 200) {
-        dispatch.workbench.update({
-          listDynamic: data?.data || [],
-        })
-      }
-    },
-
     // 我近期参与的项目统计
     async myProject(payload) {
       const data = await myProject(payload)
+      console.log('项目统计===>', data)
       if (data.code === 200) {
         dispatch.workbench.update({
           projectList: data?.data || [],
@@ -41,7 +35,33 @@ const workbench = createModel()({
       console.log('成员动态===>', data)
       if (data.code === 200) {
         dispatch.workbench.update({
-          listDynamic: data?.data || [],
+          memberList: data?.data || [],
+        })
+      } else {
+        Notify.error({ title: `失败` + data.message })
+      }
+    },
+
+    // 查询所有任务
+    async selectAllProjectPage(payload) {
+      const data = await selectAllProjectPage(payload)
+      console.log('查询所有的任务===>', data)
+      if (data.code === 200) {
+        dispatch.workbench.update({
+          allList: data?.data || [],
+        })
+      } else {
+        Notify.error({ title: `失败` + data.message })
+      }
+    },
+
+    // 查询个人任务
+    async selectProjectPage(payload) {
+      const data = await selectProjectPage(payload)
+      console.log('查询所有的任务===>', data)
+      if (data.code === 200) {
+        dispatch.workbench.update({
+          personList: data?.data || [],
         })
       } else {
         Notify.error({ title: `失败` + data.message })
