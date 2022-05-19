39ae7ab38aef7e2924793a75ba70bdff70586cc7
honggaofeng <honggaofeng@yinwang.sh.cn>
2022-05-19 10:16:34 +0800

    refactor: 优化新增/编辑弹出框字段名

--- src/components/ProjectManagement/index.jsx
+++ src/components/ProjectManagement/index.jsx
@@ -88,7 +88,7 @@ const ProjectManagement = (fun) => {
             rules: [{ required: true, message: '请输入项目负责人' }],
           },
           {
-            label: '起止日期:',
+            label: '起始日期:',
             key: 'begin',
             initialValue: seachValue?.begin,
             widget: 'dateInput',
@@ -97,7 +97,7 @@ const ProjectManagement = (fun) => {
             },
             span: '24',
             required: true,
-            rules: [{ required: true, message: '请输入起止日期' }],
+            rules: [{ required: true, message: '请输入起始日期' }],
           },
           {
             label: '截止日期:',
