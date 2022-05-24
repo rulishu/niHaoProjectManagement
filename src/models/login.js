c3d14f727b0dba42c3074eeaa1f96345d472a540
honggaofeng <honggaofeng@yinwang.sh.cn>
2022-05-24 10:19:04 +0800

    refactor: 对接gitlab第三方登录 #321

--- src/models/login.js
+++ src/models/login.js
@@ -1,28 +1,47 @@
-import { history } from '@uiw-admin/router-control';
-import { createModel } from '@rematch/core';
+import { history } from '@uiw-admin/router-control'
+import { createModel } from '@rematch/core'
+import { getThirdLoginToken, authorAndLogin } from '../servers/login'
+
 const login = createModel()({
   name: 'login',
   state: {
     userData: null,
-    token: null
+    token: null,
   },
   reducers: {
-    updateState: (state, payload) => ({ ...state,
-      ...payload
-    })
+    updateState: (state, payload) => ({
+      ...state,
+      ...payload,
+    }),
   },
-  effects: dispatch => ({
+  effects: (dispatch) => ({
     async submit() {
       dispatch.login.updateState({
-        token: '测试2'
-      });
-      sessionStorage.setItem('auth', JSON.stringify(['/home', '/dac']));
-      history.push('/home'); // this.updateState()
+        token: '测试2',
+      })
+      sessionStorage.setItem('auth', JSON.stringify(['/home', '/dac']))
+      history.push('/home') // this.updateState()
       // await login({ username: 'test', password: 'www' });
       // dispatch.sharks.increment(payload)
       // `dispatch.s` will suggest `sharks`
-    }
+    },
 
-  })
-});
-export default login;
+    async getThirdLoginToken() {
+      const data = await getThirdLoginToken()
+      // console.log(data.data)
+      // debugger
+      if (data && data.data) {
+        window.location.href = data.data.gitLabUrl
+      }
+    },
+
+    async thirdLogin(param) {
+      const data = await authorAndLogin(param)
+      if (data && data.code === 200) {
+        localStorage.setItem('token', data.data.token)
+        // dispatch.login.updateState({ token: data.data.token });
+      }
+    },
+  }),
+})
+export default login
