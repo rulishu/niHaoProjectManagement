5b1b13e727c4ea3a751f3321e0eebe727829629b
gssggssg <490104722@qq.com>
2022-06-11 17:12:16 +0800

    feat: 开发同步数据详情数据列表功能 #407

--- src/pages/Authority/Migrate/MigrateControl/MigrateControlSynch/index.jsx
+++ src/pages/Authority/Migrate/MigrateControl/MigrateControlSynch/index.jsx
@@ -2,14 +2,15 @@ import { useState, useEffect, Fragment } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useParams, useLocation } from 'react-router-dom'
 import { Card, Descriptions, Loader, Tabs, Divider } from 'uiw'
+import TabBox from './TabBox'
 import styles from './index.module.less'
 
 // 来源类型对象
 const sourceTypeObj = { 10: 'GitLab', 20: 'GitHub', 30: '禅道' }
 // 项目状态对象
-const projectStatusObj = { 0: '打开' }
+// const projectStatusObj = { 0: '打开' }
 
-const MigrateControlSynch = (props) => {
+const MigrateControlSynch = () => {
   const {
     migrate: {
       control: { dataInfo },
@@ -23,15 +24,18 @@ const MigrateControlSynch = (props) => {
 
   useEffect(() => {
     const callback = async () => {
-      await dispatch({
-        type: 'migrate/getDataByIdControl',
-        payload: synchId,
-      })
+      await dispatch.migrate.getDataByIdControl(synchId)
+      // await dispatch({
+      //   type: 'migrate/getDataByIdControl',
+      //   payload: synchId,
+      // })
     }
     callback()
   }, [dispatch, synchId])
 
-  console.log('location===>', location)
+  useEffect(() => {
+    setTabKey(location?.state?.tabKey || '1')
+  }, [location.state.tabKey, synchId])
 
   // 跳转连接块
   const jumpBox = (href) => {
@@ -66,11 +70,11 @@ const MigrateControlSynch = (props) => {
                     {jumpBox(dataInfo?.webUrl)}
                   </Descriptions.Item>
                   <Descriptions.Item label="ssh地址">
-                    {jumpBox(dataInfo?.sshUrl)}
+                    {dataInfo?.sshUrl}
                   </Descriptions.Item>
-                  <Descriptions.Item label="项目状态">
+                  {/* <Descriptions.Item label="项目状态">
                     {projectStatusObj[dataInfo?.projectStatus]}
-                  </Descriptions.Item>
+                  </Descriptions.Item> */}
                   <Descriptions.Item label="来源类型">
                     {sourceTypeObj[dataInfo?.sourceType]}
                   </Descriptions.Item>
@@ -83,20 +87,38 @@ const MigrateControlSynch = (props) => {
               <div>
                 <h2>其它信息</h2>
                 <Tabs
-                  type="card"
                   activeKey={tabKey}
                   onTabClick={(activeKey, key, e) => {
                     setTabKey(activeKey)
                   }}>
-                  <Tabs.Pane label="项目标签" key="1" />
-                  <Tabs.Pane label="项目任务" key="2" />
+                  <Tabs.Pane label="项目任务" key="1" />
+                  <Tabs.Pane label="项目标签" key="2" />
                   <Tabs.Pane label="项目角色" key="3" />
                   <Tabs.Pane label="项目里程碑" key="4" />
                 </Tabs>
-                {tabKey === '1' && <div>标签{tabKey}</div>}
-                {tabKey === '2' && <div>任务{tabKey}</div>}
-                {tabKey === '3' && <div>角色{tabKey}</div>}
-                {tabKey === '4' && <div>里程碑{tabKey}</div>}
+                {tabKey === '1' && (
+                  <div>
+                    <TabBox data={dataInfo?.issues} type="issues" />
+                  </div>
+                )}
+                {tabKey === '2' && (
+                  <div>
+                    <TabBox data={dataInfo?.labels} type="labels" />
+                  </div>
+                )}
+                {tabKey === '3' && (
+                  <div>
+                    <TabBox data={dataInfo?.projectMember} type="member" />
+                  </div>
+                )}
+                {tabKey === '4' && (
+                  <div>
+                    <TabBox
+                      data={dataInfo?.projectMilestone}
+                      type="milestone"
+                    />
+                  </div>
+                )}
               </div>
             </div>
           </Loader>
