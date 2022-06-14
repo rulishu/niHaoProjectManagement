639f8d34d17661bae60fa4a0a09d59ea45767f99
夏佳乐 <xiajiale@jz-sh.cn>
2022-06-14 16:13:17 +0800

    fix: 处理团队新增数据没清空bug #611

--- src/pages/TeamManagement/index.js
+++ src/pages/TeamManagement/index.js
@@ -129,7 +129,7 @@ export default function Index() {
                   payload: {
                     drawerType: 'add',
                     drawerVisible: true,
-                    tablePro: {},
+                    tablePro: table,
                     queryInfo: {},
                   },
                 })
