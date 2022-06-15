44b7f8e9ca3298c3ff8048eeedc8f9e00397dbfe
gssggssg <490104722@qq.com>
2022-06-15 15:02:06 +0800

    refactor: 任务详情页面评论板块重构 #590

--- src/pages/Project/TaskInfo/TaskEvent.jsx
+++ src/pages/Project/TaskInfo/TaskEvent.jsx
@@ -1,6 +1,7 @@
 // import { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { Card, Icon, Avatar, Button, Tooltip } from 'uiw'
+import MarkdownPreview from '@uiw/react-markdown-preview'
 import FromMD from './fromMD'
 import styles from './taskEvent.module.less'
 
@@ -60,6 +61,7 @@ const TaskEvent = (props) => {
   //     projectId: replyCommentData.projectId,
   //   })
   // }
+
   // const handleComment = (detail, item) => {
   //   setEditParameter(
   //     detail === 'del' || detail === 'delSub'
@@ -106,39 +108,59 @@ const TaskEvent = (props) => {
   //   }
   // }
 
+  const replyBox = (item, tier = 1) => {
+    return (
+      <div className={styles.eventLiBox} key={item.taskHistoryId}>
+        <div className={styles.eventLiIcon}>
+          <Avatar src="https://avatars2.githubusercontent.com/u/1680273?s=40&v=4" />
+        </div>
+        <div className={styles.eventLiContent}>
+          <div className={styles.messageHeader}>
+            <div className={styles.info}>{item.createName}</div>
+            <div className={styles.actions}>
+              {tier === 1 && (
+                <Tooltip placement="top" content="回复">
+                  <Button icon="message" basic size="small" type="light" />
+                </Tooltip>
+              )}
+              <Tooltip placement="top" content="删除">
+                <Button icon="delete" basic size="small" type="light" />
+              </Tooltip>
+              <Tooltip placement="top" content="编辑">
+                <Button icon="edit" basic size="small" type="light" />
+              </Tooltip>
+            </div>
+          </div>
+          <div className={styles.messageContent}>
+            {showMDBox(item.operatingRecords)}
+            {/* {editContent(item.operatingRecords)} */}
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  // 回复评论
   const reviewBox = (item) => {
     return (
       <li key={item.taskHistoryId} className={styles.message}>
         <Card>
-          <div className={styles.eventLiBox}>
-            <div className={styles.eventLiIcon}>
-              <Avatar src="https://avatars2.githubusercontent.com/u/1680273?s=40&v=4" />
+          {replyBox(item)}
+          {item.managerAssignmentHistorieList &&
+          item.managerAssignmentHistorieList?.length ? (
+            <div className={styles.replyManage}>
+              <div className=""></div>
+              {item.managerAssignmentHistorieList.map((itemA) => {
+                return <Card>{replyBox(itemA, 2)}</Card>
+              })}
             </div>
-            <div className={styles.eventLiContent}>
-              <div className={styles.messageHeader}>
-                <div className={styles.info}>{item.createName}</div>
-                <div className={styles.actions}>
-                  <Tooltip placement="top" content="回复">
-                    <Button icon="message" basic size="small" type="light" />
-                  </Tooltip>
-                  <Tooltip placement="top" content="删除">
-                    <Button icon="delete" basic size="small" type="light" />
-                  </Tooltip>
-                  <Tooltip placement="top" content="编辑">
-                    <Button icon="edit" basic size="small" type="light" />
-                  </Tooltip>
-                </div>
-              </div>
-              <div className={styles.messageContent}>
-                {item.operatingRecords}
-              </div>
-            </div>
-          </div>
+          ) : null}
         </Card>
       </li>
     )
   }
 
+  // 动态事件
   const event = (item) => {
     return (
       <li key={item.taskHistoryId}>
@@ -153,7 +175,7 @@ const TaskEvent = (props) => {
   }
 
   // MD文档编译器
-  const editContent = () => {
+  const editContent = (type) => {
     return (
       <div className={styles.editContent}>
         <FromMD
@@ -166,6 +188,15 @@ const TaskEvent = (props) => {
           tributeList={userSelectAllList}
           isComment={true}
         />
+      </div>
+    )
+  }
+
+  // 显示MD文档
+  const showMDBox = (data) => {
+    return (
+      <div data-color-mode="light" style={{ flex: 1 }}>
+        <MarkdownPreview source={data || ''} style={{ width: '100%' }} />
       </div>
     )
   }
