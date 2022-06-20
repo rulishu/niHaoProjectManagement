b4eef7e8f34623c32fe5c23b8ea5dc626476d1c5
夏佳乐 <xiajiale@jz-sh.cn>
2022-06-20 16:23:58 +0800

    fix: 更改创建人路径 #701

--- src/pages/TaskBoard/TaskDetail.jsx
+++ src/pages/TaskBoard/TaskDetail.jsx
@@ -18,11 +18,13 @@ const TaskDetail = (props) => {
     loading,
     selectBoard,
   } = props?.param
+  const {
+    routeManagement: { userData },
+  } = useSelector((state) => state)
   const dispatch = useDispatch()
   const navigate = useNavigate()
   const { taskboard } = useSelector((state) => state)
   const { taskInfo, labelList, userAllList, milepostaData } = taskboard
-
   const [assignState, setAssignState] = useState(false) //指派人组件状态
   const [labelState, setLabelState] = useState(false) //标签组件状态
   const [cLabelList, setCLabelList] = useState(taskInfo.labels) //当前选中标签
@@ -207,7 +209,7 @@ const TaskDetail = (props) => {
                   <span
                     className={styles.projectName}
                     onClick={() => {
-                      navigate(`/${taskInfo.createName}`)
+                      navigate(`/${userData?.userName}`)
                     }}>
                     {taskInfo.createName}
                   </span>{' '}
