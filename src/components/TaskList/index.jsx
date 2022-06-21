576a64ef8e7d3314ce2a1e8d71d469fba3911f1e
gssggssg <490104722@qq.com>
2022-06-21 18:02:07 +0800

    feat: 封装任务列表组件，增加共呢个据状态选择 #685

--- src/components/TaskList/index.jsx
+++ src/components/TaskList/index.jsx
@@ -1,13 +1,29 @@
 import { Icon, Tooltip, Avatar } from 'uiw'
 import timeDistance from '@/utils/timeDistance'
+import { useNavigate } from 'react-router-dom'
 import styles from './index.module.less'
 
 const TaskList = (props) => {
-  const { listData = [], labelsData = [], onCLickSearch, listNavigate } = props
+  const navigate = useNavigate()
+  const {
+    listData = [],
+    labelsData = [],
+    onCLickSearch,
+    listNavigate,
+    onTabClick,
+  } = props
 
   // 跳转路径
   const listGoTo = (val) => {
     listNavigate(val)
+  }
+
+  const goPage = (value) => {
+    navigate(`/${value}`)
+  }
+
+  const onTabClicks = (tab) => {
+    onTabClick(tab)
   }
 
   const labelBox = (value = []) => {
@@ -43,18 +59,58 @@ const TaskList = (props) => {
       <div className={styles.list}>
         <ul>
           <li className={styles.liHead}>
-            <div className={styles.itemState}>
-              <span>
+            <div className={styles.itemLeft}>
+              <span
+                className={styles.clickableDiscolor}
+                style={{ fill: '#41b3498c' }}
+                onClick={() => onTabClicks('1')}>
+                <Icon type="minus-circle-o" />
+                未开始
+              </span>
+              <span
+                className={styles.clickableDiscolor}
+                style={{ fill: '#41b3498c' }}
+                onClick={() => onTabClicks('2')}>
+                <Icon type="plus-circle-o" />
+                进行中
+              </span>
+              <span
+                className={styles.clickableDiscolor}
+                style={{ fill: '#41b3498c' }}
+                onClick={() => onTabClicks('3')}>
                 <Icon type="circle-check-o" />
+                已完成
+              </span>
+              <span
+                className={styles.clickableDiscolor}
+                style={{ fill: '#41b3498c' }}
+                onClick={() => onTabClicks('4')}>
+                <Icon type="circle-close-o" />
+                已逾期
               </span>
             </div>
-            <div className={styles.itemLeft}>标题</div>
             <div className={styles.itemRight}>
-              <div>里程碑</div>
-              <div>指派人</div>
-              <div>排序</div>
+              <div>
+                <span className={styles.clickableDiscolor}>
+                  里程碑
+                  <Icon type="down" />
+                </span>
+              </div>
+              <div>
+                <span className={styles.clickableDiscolor}>
+                  指派人
+                  <Icon type="down" />
+                </span>
+              </div>
+              <div>
+                <span className={styles.clickableDiscolor}>
+                  里程碑
+                  <Icon type="down" />
+                </span>
+              </div>
             </div>
           </li>
+
           {listData.map((item) => {
             return (
               <li key={item.assignmentId}>
@@ -74,36 +130,48 @@ const TaskList = (props) => {
                       更新于{timeDistance(item.updateTime).time}前
                     </span>
                     由
-                    <span className={styles.clickable}>{item?.updateName}</span>
+                    <span
+                      className={styles.clickable}
+                      onClick={() => goPage(`${item.assigneeUserAccount}`)}>
+                      {item?.updateName}
+                    </span>
+                    {item.milestonesId ? (
+                      <span className={styles.clickable}>
+                        <Icon type="pie-chart" />
+                        {item?.milestonesTitle}
+                      </span>
+                    ) : (
+                      ''
+                    )}
                   </div>
                 </div>
                 <div className={styles.itemRight}>
                   <div></div>
                   <div>
-                    <Tooltip content={`指派给${item?.assigneeUserName}`}>
-                      <span
-                        className={styles.clickable}
-                        onClick={() => {
-                          console.log('nihao')
-                        }}>
-                        <Avatar
-                          size="small"
-                          src={
-                            item?.assigneeUserAvatar &&
-                            `/api/file/selectFile/${item?.assigneeUserAvatar}`
-                          }
-                          className={styles.roleAvatar}>
-                          {item.assigneeUserName && item.assigneeUserName[0]}
-                        </Avatar>
-                      </span>
-                    </Tooltip>
+                    {item.assigneeUserId ? (
+                      <Tooltip content={`指派给${item?.assigneeUserName}`}>
+                        <span
+                          className={styles.clickable}
+                          onClick={() => goPage(`${item.assigneeUserAccount}`)}>
+                          <Avatar
+                            size="small"
+                            src={
+                              item?.assigneeUserAvatar &&
+                              `/api/file/selectFile/${item?.assigneeUserAvatar}`
+                            }
+                            className={styles.roleAvatar}>
+                            {item.assigneeUserName && item.assigneeUserName[0]}
+                          </Avatar>
+                        </span>
+                      </Tooltip>
+                    ) : (
+                      ''
+                    )}
                   </div>
                   <div className={styles.itemComments}>
                     <span
-                      className={styles.clickable}
-                      onClick={() => {
-                        console.log('nihao')
-                      }}>
+                      className={styles.clickableDiscolor}
+                      onClick={() => listGoTo(item)}>
                       <Icon type="message" />
                       {item?.commentNum}
                     </span>
