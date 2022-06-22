20096acc114eaabd02bfa54712b3ced49acb1a54
rulishu <rulishu@jz-sh.cn>
2022-06-22 16:35:54 +0800

    fix: 删除公用组件优化

--- src/components/DeletePopover.jsx
+++ src/components/DeletePopover.jsx
@@ -2,40 +2,6 @@ import { Modal } from 'uiw'
 
 export default function DeletePopover({ isOpen, onConfirm, onClosed }) {
   return (
-    // <Popover
-    //   trigger="click"
-    //   placement="left"
-    //   isOpen={isOpen}
-    //   onVisibleChange={setIsOpen}
-    //   content={
-    //     <Card bordered={false} style={{ width: 200 }}>
-    //       <div>是否删除该数据</div>
-    //       <div
-    //         style={{
-    //           display: 'flex',
-    //           justifyContent: 'flex-end',
-    //           marginTop: 15,
-    //         }}>
-    //         <Button
-    //           size="small"
-    //           onClick={() => {
-    //             setIsOpen(false)
-    //           }}>
-    //           取消
-    //         </Button>
-    //         <Button
-    //           type="danger"
-    //           size="small"
-    //           onClick={async () => {
-    //             const result = await handleEditTable()
-    //             result && setIsOpen(false)
-    //           }}>
-    //           确定
-    //         </Button>
-    //       </div>
-    //     </Card>
-    //   }
-    //   >
     <Modal
       title="删除提示"
       isOpen={isOpen}
@@ -48,7 +14,5 @@ export default function DeletePopover({ isOpen, onConfirm, onClosed }) {
       onClosed={onClosed}>
       <p>确认删除本条数据吗?</p>
     </Modal>
-
-    // {/* </Popover> */ }
   )
 }
